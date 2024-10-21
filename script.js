7bd4fb22dcbce1996283d5dd1da4d82a92edf789
priyeshraj20 <priyeshraj20@gmail.com>
2024-10-21 13:51:49 +0530

    Slight changes in html

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const getPalindrome = (inputText) =>{
   const orginalText = inputText; 
   const formatText = inputText.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
-   
+  if (formatText === [...formatText].reverse().join('')){
+    console.log("Palindrome");
+  }
+  else {
+    console.log("Not a Palindrome");
+  }
 }
+
+getPalindrome("Malayalam");
+getPalindrome("mal yalam");
+getPalindrome("_eye");
+getPalindrome("A man, a plan, a canal. Panama");
