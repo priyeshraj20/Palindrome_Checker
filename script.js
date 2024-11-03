beca809cb9a13281dcb47384193ce19ecd2bcf9f
priyeshraj20 <priyeshraj20@gmail.com>
2024-11-03 22:32:06 +0530

    Test cases in freecode camp is completed

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-const getPalindrome = (inputText) =>{
-  const orginalText = inputText; 
-  const formatText = inputText.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
-  if (formatText === [...formatText].reverse().join('')){
-    console.log("Palindrome");
-  }
-  else {
-    console.log("Not a Palindrome");
-  }
-}
+const userInput = document.getElementById("text-input")
+const checkPalindromeBtn = document.getElementById("check-btn");
+const resultDiv = document.getElementById("result");
 
-getPalindrome("Malayalam");
-getPalindrome("test");
-getPalindrome("mala yalam");
-getPalindrome("_eye");
-getPalindrome("A man, a plan, a canal. Panama");
-getPalindrome("not a palindrome");
-getPalindrome("0_0 (: /-\ :) 0-0");
-getPalindrome("<.>>())");
+
+const getPalindrome = inputText =>{
+  if(inputText === ""){
+    alert("Please input a value");
+    return;
+  }
+  const orginalText = inputText;
+  const formatText = inputText.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
+  let resultMsg = `<strong>${orginalText}</strong> ${
+   formatText === [...formatText].reverse().join('') ?"is" : "is not"} a palindrome`;
+  resultDiv.innerHTML = resultMsg;
+};
+
+checkPalindromeBtn.addEventListener("click", () => {
+getPalindrome(userInput.value);
+userInput.value = "";
+});
